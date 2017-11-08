df990aa41427fad8510ae9cd6daf1658b0294d22
katayama <katayama@localdomain>
2017-11-08 16:06:59 +0900

    検索結果を見やすく修正

--- search.php
+++ search.php
@@ -10,6 +10,7 @@ foreach ( $pdo->query ( 'select * from cpes;' ) as $row ) {
 $product_list = array_unique($product_list_all);
 sort($product_list);
 echo "<form method='post' action='search.php'>";
+echo "<lavel>プロダクト名</lavel><br />";
 echo "<select name='search_str'>";
 foreach ( $product_list as $product_name) {
     echo "<option value='".$product_name."'>".$product_name."</option>";
@@ -24,26 +25,28 @@ echo "<br/>";
 
 $nvd_id_list_all = "";
 $search = isset($_POST['search_str'])?$_POST['search_str']:null;
-foreach ( $pdo->query ( 'select * from cpes where product = "'.$search.'";' ) as $row ) {
-    $nvd_id_list_all[] = $row['nvd_id'];
-}
-$nvd_id_list = array_unique($nvd_id_list_all);
+if( ! is_null($search)){
+    foreach ( $pdo->query ( 'select * from cpes where product = "'.$search.'";' ) as $row ) {
+        $nvd_id_list_all[] = $row['nvd_id'];
+    }
+    $nvd_id_list = array_unique($nvd_id_list_all);
+    echo "<h4>選択プロダクト名：".$search."</h4>";
 
-echo "<tr>";
-echo "<th width='150'>CVE</th>";
-echo "<th>summary</th>";
-echo "<th width='70'>スコア</th>";
-echo "</tr>";
-foreach($nvd_id_list as $nvd_id){
-    foreach ( $pdo->query ( 'select * from nvds where id = "'.$nvd_id.'";' ) as $row ) {
-        echo "<tr>";
-        echo "<td>".$row['cve_id']."</td>";
-        echo "<td>".$row['summary']."</td>";
-        echo "<td>".$row['score']."</td>";
-        echo "</tr>";
+    echo "<tr>";
+    echo "<th width='150'>CVE</th>";
+    echo "<th>summary</th>";
+    echo "<th width='70'>スコア</th>";
+    echo "</tr>";
+    foreach($nvd_id_list as $nvd_id){
+        foreach ( $pdo->query ( 'select * from nvds where id = "'.$nvd_id.'";' ) as $row ) {
+            echo "<tr>";
+            echo "<td>".$row['cve_id']."</td>";
+            echo "<td>".$row['summary']."</td>";
+            echo "<td>".$row['score']."</td>";
+            echo "</tr>";
+        }
     }
 }
-
 
 ?>
 </table>
