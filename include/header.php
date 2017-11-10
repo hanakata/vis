4aa59b8ece4b34e5c92572c615150806c9070a86
katayama <katayama@localdomain>
2017-11-10 17:56:41 +0900

    コミット忘れ

---
+++ include/header.php
@@ -0,0 +1,24 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="ja">';
+echo '  <head>';
+echo '    <meta charset="utf-8">';
+echo '    <meta http-equiv="X-UA-Compatible" content="IE=edge">';
+echo '    <meta name="viewport" content="width=device-width, initial-scale=1">';
+echo '    <title>viser</title>';
+echo '    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">';
+echo '    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>';
+echo '    <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="utf-8"></script>';
+echo '    <script src="bootstrap/js/bootstrap.min.js"></script>';
+echo '  </head>';
+
+
+echo '<div class="row">';
+echo '    <nav class="navbar navbar-inverse navbar-fixed-top">';
+echo '        <div class="navbar-header">';
+echo '            <a href="search.php" class="navbar-brand">viser</a>';
+echo '        </div>';
+echo '        <ul class="nav navbar-nav">';
+echo '        </ul>';
+echo '    </nav>';
+echo '</div>';
